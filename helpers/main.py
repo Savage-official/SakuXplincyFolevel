5d7c16312fd056f31a659def1bc37636400c9e1d
Savage-official <106460014+Savage-official@users.noreply.github.com>
2022-05-30 03:36:31 +0530

    Create main.py

---
+++ helpers/main.py
@@ -0,0 +1,29 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN, SESSION_NAME
+from pytgcalls import PyTgCalls, idle
+
+bot = Client(
+    "Love",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "Love.Plugins"},
+    )
+
+Plincy = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_name=SESSION_NAME,
+    
+    )
+
+user = PyTgCalls(shivam,
+    cache_duration=100,
+    overload_quiet_mode=True,)
+
+call_py = PyTgCalls(shivam, overload_quiet_mode=True)
+
+with Client("Love", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
+with plincy as app:
+    me_plincy = app.get_me()
