df0b34bfae185b004ab2ef25a8431798551b9faf
Savage-official <106460014+Savage-official@users.noreply.github.com>
2022-05-30 03:19:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+SESSION_NAME = getenv("SESSION_NAME", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", "5002089615"))
+API_ID = int(getenv("API_ID", "3706077")) #oP
+API_HASH = getenv("API_HASH", "5bec73923f996b3092a4209ac556bc58")
